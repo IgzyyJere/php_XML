7fbdbea5986a1cf5622962a48f02b9f540a58416
IgorJ <igor.jercic@yandex.com>
2023-08-11 19:41:39 +0200

    Nađeno izgubljeno, APi merge XML
    Adircon age

---
+++ Api/config.php
@@ -0,0 +1,19 @@
+<?
+class Database{
+
+private $host = "localhost";
+private $db_name = "api_db";
+private $username = "root";
+private $pass = "";
+
+public $conn;
+
+public function getConnection(){
+$this-> $conn = null;  
+}
+
+
+
+}
+
+?>
